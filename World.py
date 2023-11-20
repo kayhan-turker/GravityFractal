d9bc4e00f4961e05fe698655e33ceee07f2c5d6f
Kayhan Turker <kturker92@gmail.com>
2023-11-20 16:52:27 -0500

    Fix time factor for pixel color

--- World.py
+++ World.py
@@ -63,7 +63,7 @@ class World:
         self.combine_list.clear()
 
         pixel_clr = (0, 0, 0) if hit_obj is None else self.obj_clr[hit_obj]
-        time_const = 255 / (PIXEL_CONTRAST * self.timer + 1)
+        time_const = 1.0 / (PIXEL_CONTRAST * self.timer + 1)
         pixel_clr = [round(pixel_clr[i] * time_const) for i in range(3)]
         self.grid_rectangles[self.start_x][self.start_y].color = pixel_clr
 
