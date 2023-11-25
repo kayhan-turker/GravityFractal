3e751b37ca2e256505008e15aab4bb3557cd201c
Kayhan Turker <kturker92@gmail.com>
2023-11-24 19:25:46 -0500

    change init params

--- init_params.py
+++ init_params.py
@@ -9,6 +9,6 @@ INIT_Y = np.array((0.0, 1 / 4, 1 / 2, 3 / 4, 1 / 2)) * SY
 INIT_VX = np.array((0., 0., -0., 0., -0.)) * 0
 INIT_VY = np.array((0., -0., 0., -0., 0.)) * 0
 
-INIT_CLR = np.array((CLR_WHITE, CLR_RED, CLR_YELLOW, CLR_GREEN, CLR_BLUE), dtype=np.float16)
-INIT_MASS = np.array((1.0, 1.0, 1.0, 1.0, 1.0))
+INIT_CLR = np.array((CLR_WHITE, CLR_WHITE, CLR_RED, CLR_BLACK, CLR_BLUE), dtype=np.float16)
+INIT_MASS = np.array((1.2, -1.0, -1.0, -1.0, -1.0))
 INIT_RAD = np.array((2.0, 2.0, 2.0, 2.0, 2.0))
